c6777f815dc1386df4162f3cc1806d642c20c301
Jordi Puigdellívol <jordi.p@revo.works>
2024-07-31 17:44:49 +0200

    build and run works for x86 too

--- buildAndRun.sh
+++ buildAndRun.sh
@@ -2,5 +2,6 @@
 cd TMRSwift
 swift build
 cd ..
-cp ./TMRSwift/.build/arm64-apple-macosx/debug/*.dylib ./godot/bin/
+cp ./TMRSwift/.build/$(uname -m)-apple-macosx/debug/*.dylib ./godot/bin/
+
 ~/Downloads/Godot.app/Contents/MacOS/Godot --path ./godot
