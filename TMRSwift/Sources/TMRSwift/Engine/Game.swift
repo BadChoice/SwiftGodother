252d35eff61ea232a66cf18160bee48401019810
Jordi Puigdellívol <jordi.p@revo.works>
2024-08-02 13:05:06 +0200

    wip: trying to make it work for ios

--- TMRSwift/Sources/TMRSwift/Engine/Game.swift
+++ TMRSwift/Sources/TMRSwift/Engine/Game.swift
@@ -22,3 +22,21 @@ func safeClassName(_ name:String) -> String {
     let module          = String(reflecting: Game.self).components(separatedBy: ".").first!
     return module + "." + name.components(separatedBy: ".").last!.components(separatedBy: ":").first!
 }
+
+
+#if os(Linux)
+// Code specific to Linux
+
+#elseif os(macOS)
+// Code specific to macOS
+
+#elseif os(windows)
+
+#endif
+
+
+#if canImport(UIKit)
+// Code specific to platforms where UIKit is available
+
+
+#endif
--- buildAndRun.sh
+++ buildAndRun.sh
@@ -2,10 +2,10 @@
 cd TMRSwift
 swift build
 cd ..
-cp ./TMRSwift/.build/$(uname -m)-apple-macosx/debug/*.dylib ./godot/bin/
+cp ./TMRSwift/.build/$(uname -m)-apple-macosx/debug/*.dylib ./godot/bin/macos/$(uname -m)-apple-macosx
 ~/Downloads/Godot.app/Contents/MacOS/Godot --path ./godot
 
 
 # Build library for iOS
-# xcodebuild -scheme TMRSwift -destination 'platform=iOS Simulator,OS=16.0,name=iPhone 11 Pro'
+# xcodebuild -scheme TMRSwift -destination 'platform=iOS,OS=16.0,name=iPhone 11 Pro'
 # swift build --configuration debug --destination destination.json
