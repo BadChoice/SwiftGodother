5061b8ae7839bd129b65a9f57a422c1378da2eb3
Jordi Puigdellívol <jordi.p@revo.works>
2024-08-02 17:14:36 +0200

    Added a TODO Find derived data path to build for ios

--- buildForIos.sh
+++ buildForIos.sh
@@ -4,6 +4,7 @@ cd TMRSwift
 # xcodebuild -scheme TMRSwift -sdk iphonesimulator -destination 'platform=iOS Simulator,name=iPhone 12,OS=16.0' build
 xcodebuild -scheme TMRSwift -sdk iphoneos -destination 'generic/platform=iOS' build
 cd ..
-cp /Users/jordipuigdellivol/Library/Developer/Xcode/DerivedData/TMRSwift-czzbxvfbnjtqlidblkgveglrrhul/Build/Products/Debug-iphoneos/PackageFrameworks/TMRSwift.framework/* ./godot/bin/ios
+# TODO: Find the .xcframeworks automatically
+cp -R /Users/jordipuigdellivol/Library/Developer/Xcode/DerivedData/TMRSwift-czzbxvfbnjtqlidblkgveglrrhul/Build/Products/Debug-iphoneos/PackageFrameworks/* ./godot/bin/ios
 
 
