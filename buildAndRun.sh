4f094ec39ed1e494036667a07c1a5b11bab06bf1
Jordi Puigdellívol <jordi.p@revo.works>
2024-08-02 09:56:18 +0200

    Trying to compile for ios

--- buildAndRun.sh
+++ buildAndRun.sh
@@ -4,3 +4,8 @@ swift build
 cd ..
 cp ./TMRSwift/.build/$(uname -m)-apple-macosx/debug/*.dylib ./godot/bin/
 ~/Downloads/Godot.app/Contents/MacOS/Godot --path ./godot
+
+
+# Build library for iOS
+# xcodebuild -scheme TMRSwift -destination 'platform=iOS Simulator,OS=16.0,name=iPhone 11 Pro'
+# swift build --configuration debug --destination destination.json
