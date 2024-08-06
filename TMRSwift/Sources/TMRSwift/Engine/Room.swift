ea49bb6d2dfde24d7252efbbc7a335ad10b472c7
Jordi Puigdellívol <jordi.p@revo.works>
2024-08-06 18:43:34 +0200

    Texture packer working as expected

--- TMRSwift/Sources/TMRSwift/Engine/Room.swift
+++ TMRSwift/Sources/TMRSwift/Engine/Room.swift
@@ -28,72 +28,8 @@ class Room {
         setupCamera()
         addObjects()
         addWalkPath()
-        
-        
-        testLoader()
     }
     
-    func testLoader(){
-        let tp = TexturePacker(path: "res://assets/actors/crypto/crypto.atlasc", filename:"Crypto.plist")
-        tp.load()
-        
-        let t1 = Sprite2D(texture: tp.textureNamed(name: "walk/01")!)
-        let t2 = Sprite2D(texture: tp.textureNamed(name: "walk/02")!)
-        let t3 = Sprite2D(texture: tp.textureNamed(name: "walk/03")!)
-        let t4 = Sprite2D(texture: tp.textureNamed(name: "walk/04")!)
-        let t5 = Sprite2D(texture: tp.textureNamed(name: "walk/05")!)
-        let t6 = Sprite2D(texture: tp.textureNamed(name: "walk/06")!)
-        let t7 = Sprite2D(texture: tp.textureNamed(name: "walk/07")!)
-        let t8 = Sprite2D(texture: tp.textureNamed(name: "walk/08")!)
-        
-        t1.position = Vector2(x: -500, y: 200)
-        t2.position = Vector2(x: -500, y: 200)
-        t3.position = Vector2(x: -500, y: 200)
-        t4.position = Vector2(x: -500, y: 200)
-        t5.position = Vector2(x: -500, y: 200)
-        t6.position = Vector2(x: -500, y: 200)
-        t7.position = Vector2(x: -500, y: 200)
-        t8.position = Vector2(x: -500, y: 200)
-        
-        
-        t1.globalScale = Vector2(value: 2)
-        t2.globalScale = Vector2(value: 2)
-        t3.globalScale = Vector2(value: 2)
-        t4.globalScale = Vector2(value: 2)
-        t5.globalScale = Vector2(value: 2)
-        t6.globalScale = Vector2(value: 2)
-        t7.globalScale = Vector2(value: 2)
-        t8.globalScale = Vector2(value: 2)
-        
-        node.addChild(node: t1)
-        node.addChild(node: t2)
-        node.addChild(node: t3)
-        node.addChild(node: t4)
-        node.addChild(node: t5)
-        node.addChild(node: t6)
-        node.addChild(node: t7)
-        node.addChild(node: t8)
-        
-        
-        
-        /*let tp1 = tp.textureNamed(name: "idle/00")
-        let tp2 = tp.textureNamed(name: "idle/00")
-        
-        GD.print("Textures size", tp1?.getSize(), tp2?.getSize())
-        
-        let animated = AnimatedSprite2D()
-        
-        animated.spriteFrames = SpriteFrames()
-        animated.spriteFrames?.addAnimation(anim: "idle")
-        animated.spriteFrames?.addFrame(anim: "idle", texture: tp.textureNamed(name: "idle/00"), duration: 2)
-        animated.spriteFrames?.addFrame(anim: "idle", texture: tp.textureNamed(name: "idle/01"), duration: 2)
-        
-        animated.play(name: "idle")
-        animated.globalScale = Vector2(value: 2)
-        animated.position = Vector2(x: 500, y: 0)
-        node.addChild(node: animated)*/
-        
-    }
     
     private func addPlayer(){
         player = Crypto()
