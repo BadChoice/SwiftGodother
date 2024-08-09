75039a484ed29e7729458ca1568b4c555c68d594
Jordi Puigdellívol <jordi.p@revo.works>
2024-08-09 18:29:14 +0200

    fixes build for ios

--- buildForIos.sh
+++ buildForIos.sh
@@ -4,7 +4,6 @@ cd TMRSwift
 # xcodebuild -scheme TMRSwift -sdk iphonesimulator -destination 'platform=iOS Simulator,name=iPhone 12,OS=16.0' build
 xcodebuild -scheme TMRSwift -sdk iphoneos -destination 'generic/platform=iOS' build -derivedDataPath ./build/ios/
 cd ..
-# TODO: Find the .xcframeworks automatically
-cp ./TMRSwift/.build/ios/Build/Products/Debug-iphoneos/PackageFrameworks/* ./godot/bin/ios/
+cp -R ./TMRSwift/.build/ios/Build/Products/Debug-iphoneos/PackageFrameworks/* ./godot/bin/ios/
 
 
